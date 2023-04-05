75cda0c7c36c294a10e60cb3002ccdcf9e9b0938
Merlijn De Wandel <merlijn.de.wandel@quadracon.be>
2023-04-05 14:43:39 +0200

    Cleanup

--- SecureCRT-GetHostnames.py
+++ SecureCRT-GetHostnames.py
@@ -3,10 +3,10 @@ from termcolor import colored
 
 try:
     #Select Sessions folder. This is recursive, all other folders down from here will also be queried
-    path = "C:\\001_SecureCRT\\Sessions\\"
+     path = "C:\\path\\HighestFolder"
 except Exception as catchAllConnectionException:
     print('--------------------------------------------------------')
-    print(colored(f'Failed to open {"path"}', 'red'))
+    print(colored(catchAllConnectionException, 'red'))
     print('--------------------------------------------------------')
 else:
     print('--------------------------------------------------------')
@@ -14,24 +14,23 @@ else:
     #Empty current Output.txt file
     with open(f'.\Output.txt', 'w'):
         pass
-
-try:
-    for path, dirs, files in os.walk(path):
-        for filename in files:
-            fullpath = os.path.join(path, filename)
-            with open(fullpath, 'r') as file:
-                lines = file.readlines()
-                for line in lines:
-                    if line.find('"Hostname"=') != -1:
-                        hostname = line.split('=',1)
-                        hostname = hostname[1]
-                        if len(hostname) > 1 :
-                            #Write the output to a text file
-                            with open(f'.\Output.txt', 'a') as outputFile:
-                                outputFile.write(hostname)
-except Exception as catchAllOutputException:
-    print(colored(f'Failed to open {"path"}', 'red'))
-    print('--------------------------------------------------------')    
-else:    
-    print(colored(f'The hostnames were saved to .\Output.txt',"green"))
-    print('--------------------------------------------------------')                        
+    try:
+        #os.walk makes it recursive
+        for path, dirs, files in os.walk(path):
+            for filename in files:
+                fullpath = os.path.join(path, filename)
+                with open(fullpath, 'r') as file:
+                    lines = file.readlines()
+                    for line in lines:
+                        if line.find('"Hostname"=') != -1:
+                            hostname = line.split('=',1)
+                            hostname = hostname[1]
+                            if len(hostname) > 1 :
+                                #Write the output to a text file
+                                with open(f'.\Output.txt', 'a') as outputFile:
+                                    outputFile.write(hostname)
+        print(colored(f'The hostnames were saved to .\Output.txt',"green"))
+        print('--------------------------------------------------------') 
+    except Exception as catchAllOutputException:
+        print(colored(catchAllOutputException, 'red'))
+        print('--------------------------------------------------------')                     
